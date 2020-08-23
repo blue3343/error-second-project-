6f71716cd163e3c017e28c4aa0aebc2a6317a84c
blue3343 <48236865+blue3343@users.noreply.github.com>
2020-08-23 21:19:14 +0530

    Add files via upload

---
+++ division.js
@@ -0,0 +1,3 @@
+class divisions{
+    constructor(x, y,)
+}
---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var plinko = [];
+var particles = [];
+var divisions = [];
+
+
+
+function setup() {
+  var canvas = createCanvas(400,800);
+ Ground=createSprite(200,200,10,10);
+}
+
+function draw() {
+  background(46, 139, 87);  
+  drawSprites();
+}
